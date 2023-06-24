e6e0b145f825a0a203ac329be1cb3e0a65ad6797
hieu0317 <vuminhhieu171103@gmail.com>
2023-06-24 23:36:17 +0700

    add function order detail

--- FoodShop_SWP391/src/java/controller/customerController/OrderController.java
+++ FoodShop_SWP391/src/java/controller/customerController/OrderController.java
@@ -48,7 +48,7 @@ public class OrderController extends HttpServlet{
             }
         }
         req.setAttribute("orders", filterOrders);
-        req.getRequestDispatcher("views/customer/Order.jsp").forward(req, resp);
+        req.getRequestDispatcher("views/customer/order.jsp").forward(req, resp);
     }
     
     private void sortByPrice(boolean isAsc, ArrayList<Order> orders){
